c55addd5d5abf2d9de4b056d0760d5e865862a3e
franckwillian <43866213+franckwillian@users.noreply.github.com>
2018-11-03 17:48:18 -0300

    Add files via upload

--- Placebo/js/util.js
+++ Placebo/js/util.js
@@ -1,13 +1,4 @@
-function ajax(url, payload) {
-	return new Promise(resolve => {
-		var xhr = new XMLHttpRequest();
-		xhr.open('POST', '../server/' + url + '.php', true);
-		xhr.onload = () => {
-			resolve( JSON.parse(xhr.responseText) );
-		};
-		xhr.send( JSON.stringify(payload) );
-	})
-}
+
 
 function openNav() {
 				document.getElementById("mySidenav").style.width = "100%";
